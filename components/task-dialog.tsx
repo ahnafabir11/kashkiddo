f8ff6fe62d299633ac5d2d943cbb656bd1df634d
ahnafabir <ahnafabir313@gmail.com>
2024-07-06 22:21:58 +0600

    updated shandcn components and added date picker in create task dialog

--- components/task-dialog.tsx
+++ components/task-dialog.tsx
@@ -19,9 +19,16 @@ import {
   FormControl,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import * as React from "react";
-import { addHours } from "date-fns";
+import { cn } from "@/lib/utils";
 import { useForm } from "react-hook-form";
+import { addHours, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { createNewTask } from "@/lib/actions/task";
@@ -157,13 +164,35 @@ export default function TaskDialog({ className }: TaskDialogProps) {
                   render={({ field }) => (
                     <FormItem className="flex flex-col">
                       <FormLabel>Deadline</FormLabel>
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        disabled={{ before: new Date() }}
-                        className="border rounded-md"
-                      />
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPp")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={{ before: new Date() }}
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
                       <FormMessage />
                     </FormItem>
                   )}
